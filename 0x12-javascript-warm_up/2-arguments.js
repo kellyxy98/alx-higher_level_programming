a17829cd9ae6e06491af5596f112ab88bb9d0ea5
kellyxy98 <kelechiabara98@gmail.com>
2023-04-12 02:58:03 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* a script that prints a message 
+   depending on the number of arguments passed */
+   const count = process.argv.length;
+   console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
