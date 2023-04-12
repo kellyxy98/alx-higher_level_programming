359e4d28cf4f2ffa8f6e63c67291e887413fed2e
kellyxy98 <kelechiabara98@gmail.com>
2023-04-12 04:06:03 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 /* a script that prints x times "C is fun" */
 const count = math.floor(Number(process.argv[2]));
 if (isNaN(count)) {
-	console.log('missing number of occurrences');
+  console.log('missing number of occurrences');
 } else {
   for (let i = 0; i < count; i++) {
-	  console.log('C is fun');
+     console.log('C is fun');
   }
 }
